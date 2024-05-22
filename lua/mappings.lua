6a7eef63c42cc9439b765630cc651a740542529a
dlee888 <dlee888@mit.edu>
2024-05-21 21:33:47 -0400

    my changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,13 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- remap autocomplete
+local cmp = require "cmp"
+cmp.setup {
+  mapping = cmp.mapping.preset.insert {
+    ["<Tab>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping.abort(),
+  },
+}
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,31 +8,60 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "clangd",
+        "autopep8",
+        "pyright",
+        "texlab",
+      },
+    },
+  },
   --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "bash",
+        "c",
+        "cpp",
+        "cuda",
+        "latex",
+        "make",
+        "markdown",
+        "python",
+        "yaml",
+      },
+    },
+  },
+
+  {
+    "lervag/vimtex",
+    lazy = false,
+  },
+
+  {
+    "github/copilot.vim",
+    lazy = false,
+  },
 }
